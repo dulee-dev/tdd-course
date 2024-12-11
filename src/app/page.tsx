776907893f4200b1eb4e43f8e9df9f1be98cbdda
dulee <dulee.dev@gmail.com>
2024-12-11 11:35:17 +0900

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import styles from './styles.module.css';
+import s from './styles.module.css';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Next.js와 playwright, vitest로 실무 TDD 완벽 가이드 홈</h1>
+    <main className={s.main}>
+      <h1 className={s.title}>
+        Next.js와 playwright, vitest로 실무 TDD 완벽 가이드 홈
+      </h1>
     </main>
   );
 }
